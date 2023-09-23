79541efdde5aa0287f6ecfedda14b37d55d4f0e9
Mubarak150 <mkmubarak2347@gmail.com>
2023-09-23 08:07:48 -0700

    signIn.js completed successfully.

--- signIn.js
+++ signIn.js
@@ -1,98 +1,89 @@
-    var box = ""; 
-    var alert = ""; 
-    
-    var password = document.getElementById("password");
-    var email = document.getElementById("emailAddr"); // $("#emailAddr").val(); 
-    var signIn = document.getElementById("sign-in");
-   updatesignInButton(email, password, lengthPassword, validateEmail )
-    
+document.addEventListener("DOMContentLoaded", function() {
 
-  
-    email.addEventListener("input", function(){
-      mail = this.value;
-      validateEmail(mail);
-      updatesignInButton(email, password, lengthPassword, validateEmail )
-    })
-    
-    password.addEventListener("input", function(){
-      pass = this.value;
-      alert = box.nextElementSibling;
-      lengthPassword (pass, password, alert);
-      updatesignInButton(email, password, lengthPassword, validateEmail )
-    })
+// Definitions:
+  var boolMail = false;
+  var boolPass = false;
 
-  
-    signIn.addEventListener("input",function() {
-      alert("sign in succeeded")
-    })
-  
-  
-  
-  /////////////////////////////////////////////////////// functions //
-  function lengthPassword(pass, password, alert) {
-  
-    if (password === "") {
-      alert.innerHTML("* this field is mandatory.");
-      box.styles.border = "4px double red";
-      return false;
-    } else if (password.length < 6) {
-        alert.text("Password must be at least 6 characters.");
-        box.styles.border = "4px double red";
-        return false;
-    } else if (password.length > 10) {
-        alert.text("Password cannot be longer than 10 characters.");
-        box.styles.border = "4px double red";
-        return false;
-    } else if (password.length >= 6 && password.length <= 10) {
-      alert.textContent = "";
-      box.styles.border = "4px double green";
-      return true;
-    }
-  }
+  var signIn = document.getElementById("sign-in");
+  var emailBox = document.getElementById("emailAddr");
+  var passwordBox = document.getElementById("password");
+  successNote = document.getElementById("h1").nextElementSibling;
 
-  
-  function validateEmail(inputText) {
-  
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(inputText.str.match(mailformat)) {
-  
-    email.styles.border =  "4px double green";
-      email.nextElementSibling.textContent = "";
-      return true;
-    }
-  
-    else {
-      email.styles.border = "4px double red";
-      email.nextElementSibling.innerHTML = ("* You have entered an invalid email address!");
-      return false;
-    }
-  }
-  
+  // Event for E-Mail:
+  emailBox.addEventListener("input", function(){
+    var email = emailBox.value; 
+      var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      boolMail = false;
+      if (email === "") {
+        emailBox.nextElementSibling.innerHTML = "* this field is mandatory.";
+        emailBox.style.border = "4px double red";
+        boolMail = false;
+      } 
+      else if(email.match(mailformat)) {
+          emailBox.style.border =  "4px double green";
+          emailBox.nextElementSibling.textContent = "";
+          boolMail = true;
+        }
+      
+        else {
+          emailBox.style.border = "4px double red";
+          emailBox.nextElementSibling.innerHTML = ("* You have entered an invalid email address!");
+          boolMail = false;
+        }
+ })
 
-    /////////////checking sign in ///
-    // disabling sign up button if 
-    function updatesignInButton(email, password, lengthPassword, validateEmail ) {
-      // var isMatchPasswordValid = matchPassword();
-      // var isLengthPasswordValid = lengthPassword();
-      // var isValidateEmailValid = validateEmail();
-  
-      // console.log('matchPassword return:', isMatchPasswordValid);
-      // console.log('lengthPassword return:', isLengthPasswordValid);
-      // console.log('validateEmail return:', isValidateEmailValid);
-  
-      if (email === "" || password === "" || !lengthPassword() || !validateEmail() ) {
-        signIn.disabled = true;
-        signIn.styles.backgroundColor = "red";
-        signIn.styles.color = "white";
-        signIn.styles.opacity = "0.5";
-      } else {
-        signIn.disabled = false;
-        signIn.styles.backgroundColor = "green";
-        signIn.styles.color = "white";
-        console.log("entered else");
-        signIn.styles.opacity = "1";
+     // Event for Password: 
+    passwordBox.addEventListener("input", function(){
+    var password = passwordBox.value;
+        alert = signIn.nextElementSibling;
+        boolPass = false;
+        
+      if (password === "") {
+          alert.innerHTML = "* this field is mandatory.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false; 
+      } 
+      else if (password.length < 6) {
+          alert.innerHTML = "Password must be at least 6 characters.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false;
+      } 
+      else if (password.length > 10) {
+          alert.innerHTML = "Password cannot be longer than 10 characters.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false;
       }
-    }
+       else if (password.length >= 6 && password.length <= 10) {
+          alert.textContent = "";
+          passwordBox.style.border = "4px double green";
+          boolPass = true;
+      }
+    })
+
+   // Event for Sign-In CLICK:
+signIn.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Check if both boolMail and boolPass are true
+  if (boolMail && boolPass) {
     
-    
-  
+    successNote.innerHTML= "you are successfully signed in";
+    successNote.style.border = "1px solid white";
+    successNote.style.padding = "3px";
+    successNote.style.backgroundColor = "#10B981"
+
+    // Add a delay before reloading the page
+    setTimeout(function() {
+      location.reload(); // Reload the page
+    }, 1000); // 1000 milliseconds = 1 seconds
+  }
+
+  else {
+    successNote.innerHTML= "please fill credentials as prescribed!";
+    successNote.style.border = "1px solid white";
+    successNote.style.padding = "3px";
+    successNote.style.backgroundColor = "red"
+  }
+});
+});
+
--- signUp.js
+++ signUp.js
@@ -227,3 +227,96 @@ function validateName (name, box, alert) {
   
   
 });
+
+
+
+
+/////////////////////////////////  sign up partial code    ////////////////////////////////////
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Definitions:
+  var boolMail = false;
+  var boolPass = false;
+
+  var signUp = document.getElementById("sign-up");
+  var emailBox = document.getElementById("emailAddr");
+  var passwordBox = document.getElementById("password");
+  successNote = document.getElementById("h1").nextElementSibling;
+
+  // Event for E-Mail:
+  emailBox.addEventListener("input", function(){
+    var email = emailBox.value; 
+      var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      boolMail = false;
+      if (email === "") {
+        emailBox.nextElementSibling.innerHTML = "* this field is mandatory.";
+        emailBox.style.border = "4px double red";
+        boolMail = false;
+      } 
+      else if(email.match(mailformat)) {
+          emailBox.style.border =  "4px double green";
+          emailBox.nextElementSibling.textContent = "";
+          boolMail = true;
+        }
+      
+        else {
+          emailBox.style.border = "4px double red";
+          emailBox.nextElementSibling.innerHTML = ("* You have entered an invalid email address!");
+          boolMail = false;
+        }
+ })
+
+    // Event for Password: 
+    passwordBox.addEventListener("input", function(){
+    var password = passwordBox.value;
+        alert = signUp.nextElementSibling;
+        boolPass = false;
+        
+      if (password === "") {
+          alert.innerHTML = "* this field is mandatory.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false; 
+      } 
+      else if (password.length < 6) {
+          alert.innerHTML = "Password must be at least 6 characters.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false;
+      } 
+      else if (password.length > 10) {
+          alert.innerHTML = "Password cannot be longer than 10 characters.";
+          passwordBox.style.border = "4px double red";
+          boolPass = false;
+      }
+       else if (password.length >= 6 && password.length <= 10) {
+          alert.textContent = "";
+          passwordBox.style.border = "4px double green";
+          boolPass = true;
+      }
+    })
+
+// Event for sign-up CLICK:
+signUp.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Check if both boolMail and boolPass are true
+  if (boolMail && boolPass) {
+    
+    successNote.innerHTML= "you are successfully signed up";
+    successNote.style.border = "1px solid white";
+    successNote.style.padding = "3px";
+    successNote.style.backgroundColor = "#10B981"
+
+    // Add a delay before reloading the page
+    setTimeout(function() {
+      location.reload(); // Reload the page
+    }, 1000); // 1000 milliseconds = 1 seconds
+  }
+
+  else {
+    successNote.innerHTML= "please fill credentials as prescribed!";
+    successNote.style.border = "1px solid white";
+    successNote.style.padding = "3px";
+    successNote.style.backgroundColor = "red"
+  }
+});
+});
